4e23133f6da1fbdc3d4a8f082b17d08c688b943c
Romain Estievenart <res@odoo.com>
2025-04-03 13:28:53 +0200

    Allow personnal git pages

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,6 @@ import { svelte } from '@sveltejs/vite-plugin-svelte'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "https://arcasias.github.io/AutoTOB/",
+  base: "https://blenderviking.github.io/AutoTOB/",
   plugins: [svelte()],
 })
